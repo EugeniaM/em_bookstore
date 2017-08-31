0613412f12b8f2b74f85f7bfc3941bd81f799c56
EugeniaM <maltsevae88@gmail.com>
2017-08-31 23:46:36 +0300

    Change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,8 @@
 # OrderStatus.create! id: 4, name: "delivered", display_title: "Delivered"
 # OrderStatus.create! id: 5, name: "canceled", display_title: "Canceled"
 
-Categories.delete_all
-Categories.create! id: 1, name: "Mobile Development"
-Categories.create! id: 2, name: "Photo"
-Categories.create! id: 1, name: "Web Design"
-Categories.create! id: 1, name: "Web Development"
+Category.delete_all
+Category.create! id: 1, name: "Mobile Development"
+Category.create! id: 2, name: "Photo"
+Category.create! id: 1, name: "Web Design"
+Category.create! id: 1, name: "Web Development"
